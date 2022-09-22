5401821800afeefb511c66cdd4ec32010870cfa5
David Waring <david.waring2@bbc.co.uk>
2022-09-22 09:41:23 +0100

    Add source file metadata

--- src/rt_5gms_as/context.py
+++ src/rt_5gms_as/context.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
+#
+# 5G-MAG Reference Tools: 5GMS Application Server
+# ===============================================
+#
+# File: context.py
+# License: 5G-MAG Public License (v1.0)
+# Author: David Waring
+# Copyright: (C) 2022 British Broadcasting Corporation
+#
+# For full license terms please see the LICENSE file distributed with this
+# program. If this file is missing then the license can be retrieved from
+# https://drive.google.com/file/d/1cinCiA778IErENZ3JN52VFW-1ffHpx7Z/view
+#
+# This is the 5G-MAG Reference Tools 5GMS AS application context module.
+# This file handles the class which will hold the current run-time context of
+# the AS.
+#
 '''
 5gms_as.context module
 ======================
--- src/rt_5gms_as/utils.py
+++ src/rt_5gms_as/utils.py
@@ -1,4 +1,22 @@
 #!/usr/bin/python3
+#
+# 5G-MAG Reference Tools: 5GMS Application Server
+# ===============================================
+#
+# File: utils.py
+# License: 5G-MAG Public License (v1.0)
+# Author: David Waring
+# Copyright: (C) 2022 British Broadcasting Corporation
+#
+# For full license terms please see the LICENSE file distributed with this
+# program. If this file is missing then the license can be retrieved from
+# https://drive.google.com/file/d/1cinCiA778IErENZ3JN52VFW-1ffHpx7Z/view
+#
+# This is the 5G-MAG Reference Tools 5GMS AS common utility functions.
+#
+# Functions:
+# find_executable_on_path(cmd) - Find cmd and return the absolute path
+#
 '''
 General utility functions
 '''
@@ -10,6 +28,8 @@ def find_executable_on_path(cmd):
 
     Return the str path to the cmd or None if the command doesn't exist.
     '''
+    # Uses the external `which` command.
+    # TODO: this will need enhancing to work on non-unix based systems
     result = subprocess.run(['which', cmd], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if result.returncode != 0:
         return None
