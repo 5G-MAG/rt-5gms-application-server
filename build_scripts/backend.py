6005ff845ad4f462b3fc2e4e0f14222f1aef9030
David Waring <david.waring2@bbc.co.uk>
2022-10-07 11:58:30 +0100

    Rework OpenAPI bindings generator script
    Closes 5G-MAG/rt-5gms-application-server#34
    
    Reworks the OpenAPI generator script to use the common generate_openapi script from rt-common-shared.
    Move rt-common-shared submodule to a more appropriate directory since it doesn't just hold examples
    anymore.
    Rename the old generator script so that its name doesn't clash with the common script.
    Modify the project build files to reflect the new names and directory structure.

--- build_scripts/backend.py
+++ build_scripts/backend.py
@@ -39,7 +39,7 @@ def _check_openapi():
     openapi_dir = os.path.join(rt_5gms_as_dir, 'openapi_5g')
     if not os.path.isdir(openapi_dir):
         log.info('Generating OpenAPI Python classes...')
-        result = subprocess.run([os.path.join(custom_build_dir,'generate_openapi')], stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = subprocess.run([os.path.join(custom_build_dir,'generate_5gms_as_openapi')], stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         if result.returncode != 0:
             log.error("Failed: %s", result.stderr.decode('utf-8'))
         else:
